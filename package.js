e348bedd9facfee0b1623fd3be1299b9ea4c578d
Matteo De Micheli <mat.demicheli@gmail.com>
2013-10-23 08:58:34 +0200

    Fix typo issue

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-	summary : "Responsive media / lightbox repackaged for meteor"
+    summary : "Responsive media / lightbox repackaged for meteor"
 });
 
 Package.on_use(function(api) {
-	api.use('jquery', 'client');
-	api.add_files([
-		'lib/jquery.fancybox.css',
+    api.use('jquery', 'client');
+    api.add_files([
+        'lib/jquery.fancybox.css',
         'lib/jquery.fancybox.pack.js',
         'lib/fancybox_loading.gif',
-        'lib/fancybox_loading@2x.gif'
+        'lib/fancybox_loading@2x.gif',
         'lib/fancybox_overlay.png',
         'lib/fancybox_sprite.png',
         'lib/fancybox_sprite@2x.png',
         'lib/blank.gif']
-		, 'client');
+        , 'client');
 });
